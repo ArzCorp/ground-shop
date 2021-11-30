bd33c9694ecb9cff00cc24868911d43144c989b3
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2021-11-30 07:28:01 -0600

    💄 product card component created

---
+++ src/components/ProductCard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class ProductCard extends Component {
+	render() {
+		const { name, price, img } = this.props.item
+		return (
+			<div className="product-card">
+				<figure className="product-card__img-container">
+					<img
+						src={img}
+						alt={`producto ${name}`}
+						className="product-card__img-container--img"
+					/>
+				</figure>
+				<div className="product-card__description">
+					<h3 className="product-card__description--name">{name}</h3>
+					<p className="product-card__description--price">{`$${price}`}</p>
+					<button className="product-card__description--button">
+						Agregar <i className="fas fa-shopping-cart"></i>
+					</button>
+				</div>
+			</div>
+		)
+	}
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 import Header from '../components/Header'
+import ProductCard from '../components/ProductCard'
 
 export default class Home extends Component {
 	render() {
 		return (
 			<div>
 				<Header />
+				<ProductCard
+					item={{
+						name: 'tomate',
+						price: '1500',
+						img: 'https://i.ibb.co/122VT2W/tomate.jpg',
+					}}
+				/>
 			</div>
 		)
 	}
 }
+// https://i.ibb.co/Jp2PPqC/arbejas.jpg
+// https://i.ibb.co/mCmWsnS/lechuga.jpg
