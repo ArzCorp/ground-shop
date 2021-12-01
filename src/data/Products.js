65eab5e9097c1cb1a643731b3fbef9890c1c74b2
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2021-11-30 18:30:20 -0600

    ⚡added products data

---
+++ src/data/Products.js
@@ -0,0 +1,19 @@
+const Products = [
+	{
+		name: 'Tomate',
+		price: '1500',
+		img: 'https://i.ibb.co/122VT2W/tomate.jpg',
+	},
+	{
+		name: 'Arbejas',
+		price: '2500',
+		img: 'https://i.ibb.co/Jp2PPqC/arbejas.jpg',
+	},
+	{
+		name: 'Lechuga',
+		price: '500',
+		img: 'https://i.ibb.co/mCmWsnS/lechuga.jpg',
+	},
+]
+
+export default Products
